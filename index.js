9d6009573d07a44fcfe3bd4fb0ac96103ecc793a
Kenric <kenric.chui@gmail.com>
2024-03-27 22:49:06 +1100

    modal js

---
+++ index.js
@@ -0,0 +1,32 @@
+//template_4hubsji
+//service_epxdagd
+//Sw_7IS9IwGUCe2xjn
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_epxdagd",
+      "template_4hubsji",
+      event.target,
+      "Sw_7IS9IwGUCe2xjn"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The Email service is temporarily unavailable. Please contact me directly on kenric.chui@gmail.com"
+      );
+    });
+}
+
+function toggleModal() {
+    
+}
