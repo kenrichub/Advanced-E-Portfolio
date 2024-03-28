86c0b3b797a82aad5d86c71e4fe3787fb7b30283
Kenric <kenric.chui@gmail.com>
2024-03-28 22:47:13 +1100

    dark theme

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-//template_4hubsji
-//service_epxdagd
-//Sw_7IS9IwGUCe2xjn
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
 
 function contact(event) {
   event.preventDefault();
@@ -27,12 +36,11 @@ function contact(event) {
     });
 }
 
-let isModalOpen = false
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false
-        return document.body.classList.remove("modal--open")
-    }
-    isModalOpen = true
-    document.body.classList += " modal--open";
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
